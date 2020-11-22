57ed135118906db61d4f486cc12072ff1abf639d
Walter Ocampo <fearless347@gmail.com>
2020-11-22 01:31:15 -0500

    Text dark en modal

--- resources/views/sections/contact-modal.blade.php
+++ resources/views/sections/contact-modal.blade.php
@@ -8,31 +8,31 @@
                             <div class="card-header p-3">
                                 <div class="row align-items-center">
                                     <div class="col-auto">
-                                        <h6 class="mb-0"><b>Contáctanos</b></h6>
+                                        <h6 class="mb-0 text-dark"><b>Contáctanos</b></h6>
                                     </div>
                                     <div class="col text-right"><button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span class="cross" aria-hidden="true">&times;</span> </button></div>
                                 </div>
                             </div>
                             <div class="card-body">
-                                <h6 class="card-title text-center"><b>Lorem ipsum dolor sit.</b></h6>
+                                <h6 class="card-title text-center text-dark"><b>Lorem ipsum dolor sit.</b></h6>
                                 <div class="row my-3">
                                     <div class="col">
-                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
+                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="text-dark ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
                                     </div>
                                 </div>
                                 <div class="row my-3">
                                     <div class="col">
-                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
+                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="text-dark ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
                                     </div>
                                 </div>
                                 <div class="row my-3">
                                     <div class="col">
-                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
+                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="text-dark ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
                                     </div>
                                 </div>
                                 <div class="row my-3">
                                     <div class="col">
-                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
+                                        <div class="form-group mt-1 mb-1"> <input type="text" class="input-contact form-control pl-3 " required> <label class="text-dark ml-3 form-control-placeholder" for="email">Lorem.</label> </div>
                                     </div>
                                 </div>
                                 <div class="row mt-4 mb-3">
