5312ab0a0db6e4a0733da5d553f7e9ea8f46269a
Walter Ocampo <fearless347@gmail.com>
2020-11-21 23:20:09 -0500

    Dark en seccion cultura

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,4 +1,4 @@
-<section class="w3l-footer-16" style="background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1440' height='560' preserveAspectRatio='none' viewBox='0 0 1440 560'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1085%26quot%3b)' fill='none'%3e%3cpath d='M 0%2c261 C 96%2c276.2 288%2c343.4 480%2c337 C 672%2c330.6 768%2c224 960%2c229 C 1152%2c234 1344%2c335.4 1440%2c362L1440 560L0 560z' fill='rgba(4%2c 103%2c 194%2c 1)'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1085'%3e%3crect width='1440' height='560' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");">
+<section class="w3l-footer-16">
     <div class="w3l-footer-16-main">
         <div class="container">
             <div class="row footer-p">
