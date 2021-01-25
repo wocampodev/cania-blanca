9fd9a8de73f865ebd64194f82979fe61478ee07f
Walter Ocampo <fearless347@gmail.com>
2021-01-24 22:20:49 -0500

    Ajust en banner interno

--- resources/views/sections/banner.blade.php
+++ resources/views/sections/banner.blade.php
@@ -1,3 +1,3 @@
 <section id="home" class="w3l-banner py-5">
-    <a class="btn button-style" href="{{ route('products.all') }}">Lorem, ipsum.</a>
+    <a class="btn button-style main-button" href="{{ route('products.all') }}">Lorem, ipsum.</a>
 </section>
