382135941d3dc130d6d96c30687d43a87c43ffec
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-01-28 13:09:49 -0500

    Pull

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,15 @@ Route::view('/', 'home')->name('home');
 
 Route::view('/productos', 'products')->name('products.all');
 
-Route::view('/productos/{slug}', 'product-detail')->name('product.detail');
+//Route::view('/productos/{slug}', 'product-detail')->name('product.detail');
+
+
+Route::view('/hierba-luisa', 'product-detail')->name('product.detail_hierba_luisa');
+Route::view('/mosto-verde', 'product-detail')->name('product.detail_mosto_verde');
+Route::view('/uva-italia', 'product-detail')->name('product.detail_uva_italia');
+Route::view('/macerado-damasco', 'product-detail')->name('product.detail_macerado_damasco');
+Route::view('/manzanilla', 'product-detail')->name('product.detail_manzanilla');
+Route::view('/cogollito', 'product-detail')->name('product.detail_cogollito');
+
 
 Route::view('/contacto', 'contact')->name('contact');
