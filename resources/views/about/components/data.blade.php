1680b4de0ccf83cdc6c589faa2678c15b2ee26ab
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-01-31 21:21:31 -0500

    Pull view about_us

--- resources/views/about/components/data.blade.php
+++ resources/views/about/components/data.blade.php
@@ -1,53 +1,58 @@
 <div class="aboutus-section">
     <div class="container">
         <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="aboutus">
-                    <h2 class="aboutus-title">Origen</h2>
-                    <p class="aboutus-text">A finales del siglo XIX el destilado de caña de azúcar se instala como bebida preferida y popularizada en nuestro país, esto fue por el desplazamiento de la producción de azúcar de caña de la sierra hacia la costa peruana, en la que los hacendados se vieron en la necesidad de producir una alternativa a sus cultivos de caña.
-                    </p>
-                    <a class="aboutus-more" href="{{ route('products.all') }}">Ver productos</a>
+
+            <div class="col-md-7 col-sm-6 col-xs-12">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="aboutus">
+                        <h2 class="aboutus-title">Origen</h2>
+                        <p class="aboutus-text">A finales del siglo XIX el destilado de caña de azúcar se instala como bebida preferida y popularizada en nuestro país, esto fue por el desplazamiento de la producción de azúcar de caña de la sierra hacia la costa peruana, en la que los hacendados se vieron en la necesidad de producir una alternativa a sus cultivos de caña.
+                        </p>
+                        <a class="aboutus-more" href="{{ route('products.all') }}">Ver productos</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="aboutus-banner text-center">
+                        <img src="{{ asset('./assets/images/botella-sin-fondo.png') }}" alt="">
+                    </div>
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="aboutus-banner text-center">
-                    <img src="{{ asset('./assets/images/botella-sin-fondo.png') }}" alt="">
-                </div>
-            </div>
+
             <div class="col-md-5 col-sm-6 col-xs-12">
                 <div class="feature">
                     <div class="feature-box">
                         <div class="clearfix">
-                            <div class="iconset">
-                                <img class="img-fluid" src="{{ asset('./assets/images/moche.svg') }}" alt="Iconografía Moche">
-                            </div>
+{{--                            <div class="iconset">--}}
+{{--                                <img class="img-fluid" src="{{ asset('./assets/images/moche.svg') }}" alt="Iconografía Moche">--}}
+{{--                            </div>--}}
                             <div class="feature-content">
-                                <h4 class="title-color">Misión</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, laborum.</p>
+                                <h4 class="text-color">Misión</h4>
+                                <p>Recuperar la identidad del <b>destilado del jugo de la caña de azúcar</b>, y situarlo como el mejor destilado.</p>
                             </div>
                         </div>
                     </div>
                     <div class="feature-box">
                         <div class="clearfix">
-                            <div class="iconset">
-                                <img class="img-fluid" src="{{ asset('./assets/images/moche.svg') }}" alt="Iconografía Moche">
-                            </div>
+{{--                            <div class="iconset">--}}
+{{--                                <img class="img-fluid" src="{{ asset('./assets/images/moche.svg') }}" alt="Iconografía Moche">--}}
+{{--                            </div>--}}
                             <div class="feature-content">
-                                <h4 class="title-color">Visión</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, numquam.</p>
+                                <h4 class="text-color">Visión</h4>
+                                <p>Convertir a <b>CAÑABLANCA</b> en la mejor y más grande empresa de destilado de jugo de la caña de azúcar de la Región Lambayeque y porque no decir del <b>Perú</b>, y ofrecer los mejores productos y brindar un servicio de excelencia.</p>
                             </div>
                         </div>
                     </div>
                     <div class="feature-box">
                         <div class="clearfix">
-                            <div class="iconset">
-                                <img class="img-fluid" src="{{ asset('./assets/images/moche.svg') }}" alt="Iconografía Moche">
-                            </div>
+{{--                            <div class="iconset">--}}
+{{--                                <img class="img-fluid" src="{{ asset('./assets/images/moche.svg') }}" alt="Iconografía Moche">--}}
+{{--                            </div>--}}
                             <div class="feature-content">
-                                <h4 class="title-color">Valores</h4>
-                                <p class="my-0">Lorem ipsum dolor sit amet.</p>
-                                <p class="my-0">Lorem ipsum dolor sit amet.</p>
-                                <p class="my-0">Lorem ipsum dolor sit amet.</p>
+                                <h4 class="text-color">Valores</h4>
+                                <p class="my-0">Pasión</p>
+                                <p class="my-0">Compromiso</p>
+                                <p class="my-0">Calidad</p>
+                                <p class="my-0">Innovación</p>
                             </div>
                         </div>
                     </div>
