3fd5833a3388421da7753c309a1a55436650bea1
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-02-19 19:26:23 -0500

    view culture

--- resources/views/home/components/culture.blade.php
+++ resources/views/home/components/culture.blade.php
@@ -1,15 +1,22 @@
 <section class="culture-block website gradient">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-md-12 col-lg-5 offset-lg-1 text-center">
+            <div class="col-md-12 col-lg-6 text-center">
                 <h3 class="text-white">CAÑABLANCA es identidad</h3>
                 <p class="pt-3 text-white">Nuestro fino destilado es reposado por mucho tiempo en tanques apropiados y certificados para este fin, logrando así un cuerpo y madurez propio solo de los más importantes destilados.</p>
             </div>
-            <div class="col-md-12 col-lg-5">
+            <div class="col-md-12 col-lg-6 text-center">
                 <div class="culture-mockup">
-                    <div class="screen">
-                        <div class="screen-content" style="background-image: url('assets/images/senior_sipan.jpg')"></div>
-                    </div>
+                    
+                    <figure class="figure d-none d-lg-block">
+                      <img src="{{asset('assets/images/medalla_sipan.jpg')}}" class="img-fluid rounded  w-75">
+                      
+                    </figure>
+
+
+                    <!--<div class="screen">-->
+                    <!--    <div class="screen-content" style="background-image: url('assets/images/senior_sipan.jpg')"></div>-->
+                    <!--</div>-->
                 </div>
             </div>
         </div>
