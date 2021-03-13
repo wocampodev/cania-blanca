bdee734a95eb2553901c46b2496313b6f3683d1a
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-03-12 20:23:20 -0500

    pull

--- resources/views/home/components/banner.blade.php
+++ resources/views/home/components/banner.blade.php
@@ -1,4 +1,5 @@
 <section id="home" class="w3l-banner py-5">
-    <a class="btn button-style main-button" style="background-color: #F6C547;" href="{{ route('products.all') }}">VER PRODUCTOS</a>
+    <a class="btn button-style main-button" style="background-color: #30861B;" href="{{ route('products.all') }}">VER PRODUCTOS</a>
 </section>
-<p class="text-center text-white py-4" style="font-weight: 700; font-size: 32px; background-color: #30861B;">Valle Chancay, Chiclayo - Perú</p>
+
+{{-- <p class="text-center text-white py-4" style="font-weight: 700; font-size: 32px; background-color: #30861B;">Valle Chancay, Chiclayo - Perú</p> --}}
